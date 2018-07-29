372ad41b8e44b8154cfdb871d2cb39aa995b22bd
AndyJ <andrewja01@gmail.com>
2018-07-28 20:17:39 -0700

    added append file

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ var request = require("request");
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 var fs = require("fs");
-var movieName = process.argv[3];
+
 var liriInput = process.argv[2];
 var client = new Twitter(addKeys.twitter);
 var spotify = new Spotify(addKeys.spotify);
@@ -46,35 +46,51 @@ switch (liriInput) {
 //functions
 
 //omdb function
-function movieThis() {
+function movieThis(movieName) {
+
+    var movieName = process.argv[3];
 
     if (!movieName) {
         movieName = 'Mr. Nobody';
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-    var nobodyUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&tomatoes=true&apikey=trilogy";
 
     request(queryUrl, function (error, response, body) {
 
         //console.log(queryUrl);
 
         if (!error && response.statusCode === 200) {
-            console.log(divider)
-            console.log("\nTitle: " + JSON.parse(body).Title + "\nYear: " + JSON.parse(body).Year +
+            var logMovie = (divider + "\nTitle: " + JSON.parse(body).Title + "\nYear: " + JSON.parse(body).Year +
                 "\nIMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\nRotten Tomatoes Rating: "
                 + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: "
-                + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors)
-            console.log(divider)
-        }
+                + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors + "\n" + divider)
+
+            console.log(logMovie)
+
+            fs.appendFile('log.txt', logMovie, function (err) {
+                if (err) {
+                    return console.log(err);
+                } return;
+
+            });
+        };
     });
 };
 
 //twitter function
+//did not recieve twitter key in email yet
+
 function myTweets(params) {
-    var params = {screen_name: ''};
-    client.get('statuses/user_timeline', params, function(error, tweets, response){
-        if(!error) {
-            console.log(tweets);
+    var params = { screen_name: '' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(divider + "\nMost Recent Tweets: " + "\n" + tweets + "\n" + divider);
+
+            fs.appendFile('log.txt', tweets, function (err) {
+                if (err) {
+                    return console.log(err);
+                } return;
+            });
         }
     })
 }
@@ -82,24 +98,30 @@ function myTweets(params) {
 function spotifySong(trackTitle) {
 
     var trackTitle = (process.argv[3]);
-    if (!trackTitle){
+    if (!trackTitle) {
         trackTitle = 'The Sign';
     }
     //console.log(trackTitle);
     spotify.search({
         type: 'track',
         query: trackTitle.slice(),
-        limit: 10
+        limit: 1
     }, function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            for (var i = 0; i < data.tracks.items.length; i++) {
-                var songResponse = data.tracks.items[i];
-                console.log(divider);
-                console.log( divider + "Artist: " + songResponse.artists[0].name + "\nSong: " + songResponse.name + "\nLink: " + songResponse.preview_url + "Album: " + songResponse.album.name + divider);
-            }
-        })
+        if (err) {
+            return console.log(err);
+        }
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            var songResponse = data.tracks.items[i];
+            var logSongs = (divider + "\nArtist: " + songResponse.artists[0].name + "\nSong: " + songResponse.name + "\nLink: " + songResponse.preview_url + "\nAlbum: " + songResponse.album.name + "\n" + divider);
+            console.log(logSongs);
+
+            fs.appendFile('log.txt', logSongs, function (err) {
+                if (err) {
+                    return console.log(err);
+                } return;
+            })
+        }
+    })
 };
 
 //readfile function
@@ -108,7 +130,31 @@ function readFile() {
         if (error) {
             return console.log(error);
         }
-        console.log(data);
+        var doSomething = data.split(',');
+        //console.log(doSomething[1]);
+        trackTitle = doSomething[1].slice();
+        spotify.search({
+            type: 'track',
+            query: trackTitle,
+            limit: 1
+        }, function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songResponse = data.tracks.items[i];
+                var logSongs = (divider + "\nArtist: " + songResponse.artists[0].name + "\nSong: " + songResponse.name + "\nLink: " + songResponse.preview_url + "\nAlbum: " + songResponse.album.name + "\n" + divider);
+
+                console.log(logSongs);
+
+                fs.appendFile('log.txt', logSongs, function (err) {
+                    if (err) {
+                        return console.log(err);
+                    } return;
+                })
+            }
+        });
     });
+
 };
 
